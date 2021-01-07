d8cc3645e2e43cfe81cd604c30ce9905db14eff7
DUYANH <duyanh3897@gmail.com>
2021-01-07 17:08:51 +0700

    add-CORS

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from src.app.helper.connect_cache import app
 from src.app.Controller.Controllers import MerchantIds
 from flask_cors import CORS
+import logging
 
 app.register_blueprint(MerchantIds)
+logging.basicConfig(level=logging.INFO)
+logging.getLogger('flask_cors').level = logging.DEBUG
 
-CORS(app)
+CORS(app, resources=r'/api/*')
 
 if __name__ == "__main__":
     app.run(debug=True)
--- src/app/Controller/Controllers.py
+++ src/app/Controller/Controllers.py
@@ -5,10 +5,14 @@ from src.app.helper.connect_redis import *
 from src.app.helper.key_config import *
 import string
 import random
-from flask import Blueprint, Response, request
+from flask import Blueprint, Response, request, jsonify
 from bson.json_util import dumps
+import logging
+
+
 
 MerchantIds = Blueprint('MerchantIds', __name__)
+
 
 @MerchantIds.route('/')
 def get_merchantId():
@@ -72,3 +76,88 @@ def get_code_id(code):
     }
     CacheClient().set_cache(key_code, data, 10)
     return data
+
+@MerchantIds.route("/api/v1/users/")
+def list_users():
+    """
+        Since the path matches the regular expression r'/api/*', this resource
+        automatically has CORS headers set. The expected result is as follows:
+        $ curl --include -X GET http://127.0.0.1:5000/api/v1/users/ \
+            --header Origin:www.examplesite.com
+        HTTP/1.0 200 OK
+        Access-Control-Allow-Headers: Content-Type
+        Access-Control-Allow-Origin: *
+        Content-Length: 21
+        Content-Type: application/json
+        Date: Sat, 09 Aug 2014 00:26:41 GMT
+        Server: Werkzeug/0.9.4 Python/2.7.8
+        {
+            "success": true
+        }
+    """
+    return jsonify(user="j232332oe")
+
+@app.route("/api/v1/users/create", methods=['POST'])
+def create_user():
+    """
+        Since the path matches the regular expression r'/api/*', this resource
+        automatically has CORS headers set.
+        Browsers will first make a preflight request to verify that the resource
+        allows cross-origin POSTs with a JSON Content-Type, which can be simulated
+        as:
+        $ curl --include -X OPTIONS http://127.0.0.1:5000/api/v1/users/create \
+            --header Access-Control-Request-Method:POST \
+            --header Access-Control-Request-Headers:Content-Type \
+            --header Origin:www.examplesite.com
+        >> HTTP/1.0 200 OK
+        Content-Type: text/html; charset=utf-8
+        Allow: POST, OPTIONS
+        Access-Control-Allow-Origin: *
+        Access-Control-Allow-Headers: Content-Type
+        Access-Control-Allow-Methods: DELETE, GET, HEAD, OPTIONS, PATCH, POST, PUT
+        Content-Length: 0
+        Server: Werkzeug/0.9.6 Python/2.7.9
+        Date: Sat, 31 Jan 2015 22:25:22 GMT
+        $ curl --include -X POST http://127.0.0.1:5000/api/v1/users/create \
+            --header Content-Type:application/json \
+            --header Origin:www.examplesite.com
+        >> HTTP/1.0 200 OK
+        Content-Type: application/json
+        Content-Length: 21
+        Access-Control-Allow-Origin: *
+        Server: Werkzeug/0.9.6 Python/2.7.9
+        Date: Sat, 31 Jan 2015 22:25:04 GMT
+        {
+          "success": true
+        }
+    """
+    return jsonify(success=True)
+
+@app.route("/api/exception")
+def get_exception():
+    """
+        Since the path matches the regular expression r'/api/*', this resource
+        automatically has CORS headers set.
+        Browsers will first make a preflight request to verify that the resource
+        allows cross-origin POSTs with a JSON Content-Type, which can be simulated
+        as:
+        $ curl --include -X OPTIONS http://127.0.0.1:5000/api/exception \
+            --header Access-Control-Request-Method:POST \
+            --header Access-Control-Request-Headers:Content-Type \
+            --header Origin:www.examplesite.com
+        >> HTTP/1.0 200 OK
+        Content-Type: text/html; charset=utf-8
+        Allow: POST, OPTIONS
+        Access-Control-Allow-Origin: *
+        Access-Control-Allow-Headers: Content-Type
+        Access-Control-Allow-Methods: DELETE, GET, HEAD, OPTIONS, PATCH, POST, PUT
+        Content-Length: 0
+        Server: Werkzeug/0.9.6 Python/2.7.9
+        Date: Sat, 31 Jan 2015 22:25:22 GMT
+    """
+    raise Exception("example")
+
+@app.errorhandler(500)
+def server_error(e):
+    logging.exception('An error occurred during a request. %s', e)
+    return "An internal error occured", 500
