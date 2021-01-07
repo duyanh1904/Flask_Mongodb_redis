01614709d3ec1f57def0873820fff454446923a5
DUYANH <duyanh3897@gmail.com>
2021-01-07 21:14:11 +0700

    Update__2015

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from src.app.helper.connect_cache import app
-import pymongo
-from src.app.Model.base_model import Base_model
 from src.app.Controller.Controllers import MerchantIds
 from flask_cors import CORS
 import logging
@@ -9,7 +7,8 @@ app.register_blueprint(MerchantIds)
 logging.basicConfig(level=logging.INFO)
 logging.getLogger('flask_cors').level = logging.DEBUG
 
-CORS(app, resources=r'/api/*')
+
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 if __name__ == "__main__":
     app.run(debug=True)
