d62e161eee92c5d61084f785d4b966aa99998370
DUYANH <duyanh3897@gmail.com>
2021-01-07 11:41:51 +0700

    message

--- main.py
+++ main.py
@@ -1,95 +1,13 @@
-import pymongo
-from bson.objectid import ObjectId
-from flask import Flask, render_template, redirect, url_for, request
-from pymongo import MongoClient
-import string
-import random
-from flask_paginate import Pagination
-from key_config import KeyCacheRedis
-from connect_redis import has_key, set_string, get_string
-from connect_cache import cache, CacheClient
+from flask import Flask
+from flask import Blueprint
+from flask_restful import Api
+from src.app.Controller.route import *
 
 app = Flask(__name__)
 
-client = MongoClient("mongodb://127.0.0.1:27017")
-db = client.mydb
-table = db.mytable
-
-@app.route('/')
-def home():
-    search = False
-    q = request.args.get('q')
-    if q:
-        search = True
-    page = int(request.args.get('page', 1))
-    per_page = 4
-    offset = (page - 1) * per_page
-    code_list = table.find().sort("_id",pymongo.DESCENDING)
-    files_for_render = code_list.limit(per_page).skip(offset)
-    pagination = Pagination(page=page, total=code_list.count(), record_name = code_list, search = search,
-                            css_framework='bootstrap3', per_page = 4, offset = offset)
-    return render_template("base.html", code_list=files_for_render, pagination = pagination)
-
-@app.route('/code/<string:code>', methods=['GET'])
-def get_code_id(code):
-    key_code = KeyCacheRedis.KEY_CODE + str(code)
-    # check key
-    if has_key(key_code):
-        value_code = get_string(key_code)
-        print("co key: ", value_code)
-    else:
-        code_detail = table.find_one({'code': code})
-        print("code_detail: ", code_detail)
-        value_code = code_detail.get('code')
-        print("value_code: ", value_code)
-    set_string(key_code, value_code)
-
-    cached = CacheClient.get_cache(key_code)
-    print('cached: ', cached)
-    if cached:
-        print("cached====================:", cached)
-        return cached
-    chi_tiet_code = table.find_one({'code': code})
-    print("chi_tiet_code: ", chi_tiet_code)
-    id_user = chi_tiet_code.get('_id')
-    print(id_user)
-    code_user = chi_tiet_code.get('code')
-    print('code_user: ', code_user)
-    data = {
-        '_id': str(id_user),
-        'code': code_user
-    }
-    CacheClient.set_cache(key_code, data)
-    return data
-
-@app.route("/add", methods = ['POST'])
-def add():
-    def id_generator(size=9, chars=string.ascii_uppercase + string.digits):
-        return ''.join(random.choice(chars) for _ in range(size))
-    table.insert_one({"code": id_generator()})
-    return redirect('/')
-
-
-@app.route("/update")
-def updateRoute():
-    id = request.values.get("_id")
-    code_list = table.find({"_id": ObjectId(id)})
-    return render_template('update.html', code_list=code_list)
-
-
-@app.route("/action_update",methods=['post'])
-def update():
-    code = request.values.get("code")
-    id = request.values.get("_id")
-    table.update({"_id": ObjectId(id)}, {'$set': {"code": code}})
-    return redirect(url_for("home"))
-
-@app.route("/delete")
-def delete():
-    id = request.values.get("_id")
-    table.remove({"_id": ObjectId(id)})
-    return redirect(url_for("home"))
-
+app.register_blueprint(MerchantIds)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
---
+++ src/app/Controller/Controllers.py
@@ -0,0 +1,71 @@
+import pymongo
+from bson.objectid import ObjectId
+from src.app.Model.connectMongo import *
+from src.app.Model.connect_cache import *
+from src.app.Model.connect_redis import *
+from src.app.Model.key_config import *
+import string
+import random
+from flask import render_template, redirect, url_for, jsonify, Blueprint, Response, request
+from bson.json_util import dumps
+
+MerchantIds = Blueprint('MerchantIds', __name__)
+
+@MerchantIds.route('/')
+def get_merchantId():
+    merchantId = table.find()
+    code = list(merchantId)
+    data = dumps(code)
+    return Response(data, mimetype="application/json", status=200)
+
+@MerchantIds.route('/add', methods=['POST'])
+def add():
+    def id_generator(size=9, chars=string.ascii_uppercase + string.digits):
+        return ''.join(random.choice(chars) for _ in range(size))
+    table.insert_one({"code": id_generator()})
+    return 'add success', 200
+
+@MerchantIds.route("/update/<_id>",methods=['PUT'])
+def update(_id):
+    id = _id
+    code = request.form['code']
+    table.update({"_id": ObjectId(id)}, {'$set': {"code": code}})
+    return 'update success', 200
+
+@MerchantIds.route("/delete/<_id>",methods=['DELETE'])
+def delete(_id):
+    id = _id
+    table.remove({"_id": ObjectId(id)})
+    return 'delete success', 200
+
+@MerchantIds.route('/code/<string:code>', methods=['GET'])
+def get_code_id(code):
+    key_code = KeyCacheRedis.KEY_CODE + str(code)
+    # check key
+    if has_key(key_code):
+        value_code = get_string(key_code)
+        print("co key: ", value_code)
+    else:
+        code_detail = table.find_one({'code': code})
+        print("code_detail: ", code_detail)
+        value_code = code_detail.get('code')
+        print("value_code: ", value_code)
+    set_string(key_code, value_code)
+
+    cached = CacheClient().get_cache(key_code)
+    print('cached: ', cached)
+    if cached:
+        print("cached====================:", cached)
+        return cached
+    chi_tiet_code = table.find_one({'code': code})
+    print("chi_tiet_code: ", chi_tiet_code)
+    id_user = chi_tiet_code.get('_id')
+    print(id_user)
+    code_user = chi_tiet_code.get('code')
+    print('code_user: ', code_user)
+    data = {
+        '_id': str(id_user),
+        'code': code_user
+    }
+    CacheClient().set_cache(key_code, data)
+    return data
