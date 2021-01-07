9e5dd5ff2f5038c5b6ba142818aaadac5472999a
DUYANH <duyanh3897@gmail.com>
2021-01-07 16:40:46 +0700

    add-CORS

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from src.app.helper.connect_cache import app
 from src.app.Controller.Controllers import MerchantIds
+from flask_cors import CORS
 
 app.register_blueprint(MerchantIds)
+
+CORS(app)
 
 if __name__ == "__main__":
     app.run(debug=True)
