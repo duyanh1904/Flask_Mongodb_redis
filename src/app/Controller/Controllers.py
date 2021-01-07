439f31ebd484fc052819419a06dcae70f470eb2b
DUYANH <duyanh3897@gmail.com>
2021-01-07 21:21:26 +0700

    Update_next

--- src/app/Controller/Controllers.py
+++ src/app/Controller/Controllers.py
@@ -7,8 +7,7 @@ import string
 import random
 from flask import Blueprint, Response, request, jsonify
 from bson.json_util import dumps
-import pymongo
-import logging
+
 
 MerchantIds = Blueprint('MerchantIds', __name__)
 
@@ -27,7 +26,7 @@ def add():
     Base_model.table.insert_one({"code": id_generator()})
     return 'add success', 200
 
-@MerchantIds.route("/api/v1/update/<_id>", methods=['PUT'])
+@MerchantIds.route("/api/update/<_id>", methods=['PUT'])
 def update(_id):
     code = request.form['code']
     Base_model.table.update({"_id": ObjectId(_id)}, {'$set': {"code": code}})
@@ -39,7 +38,6 @@ def delete(_id):
     id = _id
     Base_model.table.remove({"_id": ObjectId(id)})
     return 'delete success', 200
-
 
 @MerchantIds.route('/code/<string:code>', methods=['GET'])
 def get_code_id(code):
@@ -73,41 +71,7 @@ def get_code_id(code):
     CacheClient().set_cache(key_code, data, 10)
     return data
 
-@MerchantIds.route("/api/v1/users/create", methods=['PUT'])
-def create_user():
-    """
-        Since the path matches the regular expression r'/api/*', this resource
-        automatically has CORS headers set.
-        Browsers will first make a preflight request to verify that the resource
-        allows cross-origin POSTs with a JSON Content-Type, which can be simulated
-        as:
-        $ curl --include -X OPTIONS http://127.0.0.1:5000/api/v1/users/create \
-            --header Access-Control-Request-Method:POST \
-            --header Access-Control-Request-Headers:Content-Type \
-            --header Origin:www.examplesite.com
-        >> HTTP/1.0 200 OK
-        Content-Type: text/html; charset=utf-8
-        Allow: POST, OPTIONS
-        Access-Control-Allow-Origin: *
-        Access-Control-Allow-Headers: Content-Type
-        Access-Control-Allow-Methods: DELETE, GET, HEAD, OPTIONS, PATCH, POST, PUT
-        Content-Length: 0
-        Server: Werkzeug/0.9.6 Python/2.7.9
-        Date: Sat, 31 Jan 2015 22:25:22 GMT
-        $ curl --include -X POST http://127.0.0.1:5000/api/v1/users/create \
-            --header Content-Type:application/json \
-            --header Origin:www.examplesite.com
-        >> HTTP/1.0 200 OK
-        Content-Type: application/json
-        Content-Length: 21
-        Access-Control-Allow-Origin: *
-        Server: Werkzeug/0.9.6 Python/2.7.9
-        Date: Sat, 31 Jan 2015 22:25:04 GMT
-        {
-          "success": true
-        }
-    """
-    return jsonify(success=True)
+
 
 
 
