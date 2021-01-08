8dcc29c5d05625c6042230b5279969698e729d1f
DUYANH <duyanh3897@gmail.com>
2021-01-08 10:19:38 +0700

    Update

--- src/app/Model/base_model.py
+++ src/app/Model/base_model.py
@@ -6,7 +6,5 @@ class Base_model():
     client = MongoClient("mongodb://127.0.0.1:27017")
     db = client.DB
     table = db.merchant_id_table
-    db[table].create_index(
-        [("code", pymongo.ASCENDING)],
-        unique=True
-    )
+    unique_index = table.create_index([('code', pymongo.ASCENDING)], unique = True)
+
