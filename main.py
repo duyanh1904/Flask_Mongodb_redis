2c18cbd52ccf76d5bf9f95144b5bf477302c60c9
DUYANH <duyanh3897@gmail.com>
2021-01-11 16:25:20 +0700

    update

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from src.app.helper.connect_cache import app
 from src.app.Controller.Controllers import MerchantIds
 from flask_cors import CORS
-from flask_restful import Api
 
 
 import logging
@@ -9,7 +8,7 @@ import logging
 app.register_blueprint(MerchantIds)
 logging.basicConfig(level=logging.INFO, filename = "global.log")
 logging.getLogger('flask_cors').level = logging.DEBUG
-api = Api(app)
+
 
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
